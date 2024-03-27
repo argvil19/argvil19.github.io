a4616ab971dd979b24de144e33d6d23ad10dc083
Argenis Villasmil <argvil19@gmail.com>
2024-03-26 22:03:06 -0300

    add google tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,20 +19,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-2ZVL382XEC"
-      ></Script>
-      <Script>
-        {`
+      <head>
+        <Script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-59G47CNB');
+        `}
+        </Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2ZVL382XEC"
+        ></Script>
+        <Script>
+          {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'G-2ZVL382XEC');
         `}
-      </Script>
+        </Script>
+      </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-59G47CNB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <div></div>
         <ParticlesComponent />
         <MainLayout>{children}</MainLayout>
